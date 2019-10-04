e6746ea206b701a4e71cd18ec25af10d918e917c
unnatna <evmakarov@inbox.ru>
2019-10-04 17:02:21 +0300

    JavaCore Level 1 Tasks 01 - 15

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(String s) {
+
+    }
+
+    public static void print(String s, int i) {
+
+    }
+
+    public static void print(String s, String st) {
+
+    }
+
+    public static void print(boolean isBool) {
+
+    }
+
+    public static void print() {
+
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task12.task1210;
+
+/* 
+Три метода и максимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static int max(int x, int y) {
+        return (x >= y)?x:y;
+    }
+
+    public static long max(long x, long y) {
+        return (x >= y)?x:y;
+    }
+
+    public static double max(double x, double y) {
+        return (x >= y)?x:y;
+    }
+
+}
