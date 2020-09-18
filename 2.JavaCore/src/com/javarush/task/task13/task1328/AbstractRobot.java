c1234a0bd1007ceedc75be100c1cbf11ac382448
UnNat_NA <evmakarov@inbox.ru>
2020-09-18 13:15:07 +0300

    JavaCore Quest Level 3 Task 28

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1328/AbstractRobot.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task13.task1328;
+
+public abstract class AbstractRobot implements Attackable, Defensable{
+    private static int hitCount;
+
+    public BodyPart attack() {
+        BodyPart attackedBodyPart = null;
+        hitCount = hitCount % 4 + 1;
+
+        if (hitCount == 1) {
+            attackedBodyPart = BodyPart.ARM;
+        } else if (hitCount == 2) {
+            attackedBodyPart = BodyPart.CHEST;
+        } else if (hitCount == 3) {
+            attackedBodyPart = BodyPart.HEAD;
+        } else if (hitCount == 4) {
+            attackedBodyPart = BodyPart.LEG;
+        }
+
+        return attackedBodyPart;
+    }
+
+    public BodyPart defense() {
+        BodyPart defendedBodyPart = null;
+        hitCount = hitCount % 4 + 2;
+
+        if (hitCount == 1) {
+            defendedBodyPart = BodyPart.HEAD;
+        } else if (hitCount == 2) {
+            defendedBodyPart = BodyPart.CHEST;
+        } else if (hitCount == 3) {
+            defendedBodyPart = BodyPart.LEG;
+        } else if (hitCount == 4) {
+            defendedBodyPart = BodyPart.ARM;
+        } else
+            defendedBodyPart = BodyPart.CHEST;
+
+        return defendedBodyPart;
+    }
+}
